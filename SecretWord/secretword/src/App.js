782b847a66666d187b9fc043fff871af90b93423
ViniciusHiroki <viniciushiroki1@gmail.com>
2023-11-22 17:46:53 -0300

    add

--- SecretWord/secretword/src/App.js
+++ SecretWord/secretword/src/App.js
@@ -1,12 +1,65 @@
-import logo from './logo.svg';
+//CSS
 import './App.css';
 
+//React
+import { useCallback, useEffect, useState } from 'react';
+
+//data
+import { wordlist } from './data/words';
+
+//components
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  {id: 1, name: "start"},
+  {id: 1, name: "game"},
+  {id: 3, name: "end"},
+]
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordlist);
+
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).lenght)];
+    const word = words[category][Math.floor(Math.random() * words[category].lenght)];
+  
+    return {word, category}
+  }
+
+  const startGame = () => {
+    const { word, category } = pickWordAndCategory();
+
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
+
+    setGameStage(stages[1].name)
+  }
+
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  }
+
+  const retry = () => {
+    setGameStage(stages[0].name);
+  }
+
   return (
     <div className="App">
-      <StartScreen/>
+      {gameStage === 'start' && <StartScreen startGame={startGame}/> }
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter}/> }
+      {gameStage === 'end' && <GameOver retry={retry}/> }
     </div>
   );
 }
---
+++ SecretWord/secretword/src/data/words.js
@@ -0,0 +1,12 @@
+export const wordlist = {
+    animais: ["Leão", "Elefante", "Girafa", "Cachorro", "Gato"],
+    cores: ["Vermelho", "Azul", "Verde", "Amarelo", "Roxo"],
+    países: ["Brasil", "Estados Unidos", "França", "Japão", "Austrália"],
+    profissões: ["Médico", "Professor", "Engenheiro", "Advogado", "Cozinheiro"],
+    alimentos: ["Pizza", "Maçã", "Arroz", "Chocolate", "Sorvete"],
+    esportes: ["Futebol", "Basquete", "Tênis", "Natação", "Ciclismo"],
+    filmes: ["Titanic", "Matrix", "Star Wars", "Jurassic Park", "O Senhor dos Anéis"],
+    roupas: ["Camiseta", "Calça", "Vestido", "Sapato", "Chapéu"],
+    instrumentos: ["Violino", "Guitarra", "Piano", "Trompete", "Bateria"],
+    transporte: ["Avião", "Bicicleta", "Navio", "Ônibus", "Trem"]
+  };
